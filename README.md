b1b6a57d28e29591606a9a60b154b785a378978c
Charlie Mooney <cmooney3@gmail.com>
2018-07-18 11:22:53 -0700

    Updated with new MAC's and now compatible w/ Cron
    This is better than having my own trackerd running all the time
    since credentials run out/etc and it's a pita to manage it that way.
    With a cronjob taking readings every 2 minutes it's a lot more robust.
    
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wifi_presence_tracker
+
+You need to install a crontab to run take_reading.py as frequently as you want.  Then run server.py in a screen session (or similar) to host the website
--- known_hosts.py
+++ known_hosts.py
@@ -1,26 +1,26 @@
 
 uninteresting_hosts = [
+  "54:60:09:E1:D6:6A", # AC's Room Chromecast Audio
   "C8:3A:6B:2E:70:22", # Charlie's Roku
   "20:CF:30:F1:50:59", # Charlie's Desktop
   "E4:F0:42:3C:15:BD", # John's Room Google Home Mini
   "08:9E:08:FE:10:C1", # Google Wifi Test
   "9C:AD:EF:61:9A:48", # John's VOIP Phone
-  "54:60:09:EC:FA:A4", # Kitchen Chromecast Audio
-  "54:60:09:F1:F5:AE", # Bathroom Chromecast Audio
-  "54:60:09:E9:37:EC", # John's Room Chromecast Audio
+  "54:60:09:EC:FA:A4", # Living Room Chromecast Audio
+  "54:60:09:F1:F5:AE", # Kitchen Chromecast Audio
+  "54:60:09:E9:37:EC", # Garage Chromecast Audio
   "D8:5D:E2:16:1C:58", # John's Printer
   "48:D6:D5:38:E8:56", # Living Room Chromecast
   "40:99:22:01:AB:4B", # John's Playstation 4 - Wifi
   "F8:46:1C:BF:0F:2A", # John's Playstation 4 - Wired
   "CC:B0:DA:1E:D1:77", # John's Roku TV
-  "0C:62:A6:0C:BD:26", # Frank's Roku TV
   "70:3A:CB:18:98:D1", # Google Wifi Router
-  "40:16:7e:67:08:57", # John's Workshop Desktop - Ziggy
+  "40:16:7E:67:08:57", # John's Workshop Desktop - Ziggy
   "14:B3:1F:1D:69:0E", # John's Room Desktop - Chester
-  "5C:52:1E:66:5A:51", # Frank's Nintendo Switch
   "F0:27:2D:37:CB:79", # John's Kindle
   "F0:4F:7C:B8:18:1A", # Ashley's Kindle
   "5C:CF:7F:E4:59:ED", # John's DIY Wifi Light
+  "54:60:09:C0:2A:02", # John's Room Chromecast Audio
 ]
 
 names = {
@@ -33,28 +33,27 @@ names = {
   "20:68:9D:5B:BB:56": "John's Laptop",
   "8C:F5:A3:B2:FC:E3": "Emma's Phone",
 
-  # Observed but unknow devices
-  "DC:66:72:C4:F0:BB": "Armando's Phone?",
-  "2C:33:61:93:0B:C7": "Frank's Phone?",
+  # Unknown devices
+  "C0:D2:F3:25:AE:04": "John's Room TV?",
 
   # Known, but boring devices
-  "5C:52:1E:66:5A:51": "Frank's Nintendo Switch",
+  "54:60:09:E1:D6:6A": "AC's Room Chromecast Audio",
+  "54:60:09:E9:37:EC": "Garage Chromecast Audio",
+  "54:60:09:C0:2A:02": "John's Room Chromecast Audio",
   "C8:3A:6B:2E:70:22": "Charlie's Roku",
   "20:CF:30:F1:50:59": "Charlie's Desktop",
   "E4:F0:42:3C:15:BD": "John's Room Google Home Mini",
   "08:9E:08:FE:10:C1": "Google Wifi Test",
   "9C:AD:EF:61:9A:48": "John's VOIP Phone",
-  "54:60:09:EC:FA:A4": "Kitchen Chromecast Audio",
-  "54:60:09:F1:F5:AE": "Bathroom Chromecast Audio",
-  "54:60:09:E9:37:EC": "John's Room Chromecast Audio",
-  "40:16:7e:67:08:57": "John's Workshop Desktop - Ziggy",
+  "54:60:09:EC:FA:A4": "Living Room Chromecast Audio",
+  "54:60:09:F1:F5:AE": "Kitchen Chromecast Audio",
+  "40:16:7E:67:08:57": "John's Workshop Desktop - Ziggy",
   "14:B3:1F:1D:69:0E": "John's Room Desktop - Chester",
   "D8:5D:E2:16:1C:58": "John's Printer",
   "48:D6:D5:38:E8:56": "Living Room Chromecast",
   "40:99:22:01:AB:4B": "John's Playstation 4 - Wifi",
   "F8:46:1C:BF:0F:2A": "John's Playstation 4 - Wired",
   "CC:B0:DA:1E:D1:77": "John's Roku TV",
-  "0C:62:A6:0C:BD:26": "Frank's Roku TV",
   "70:3A:CB:18:98:D1": "Google Wifi Router",
   "F0:27:2D:37:CB:79": "John's Kindle",
   "F0:4F:7C:B8:18:1A": "Ashley's Kindle",
